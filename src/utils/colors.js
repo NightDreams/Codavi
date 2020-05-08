53a1b943d8334a13c0b7acf598b5e0a247a1f316
Jared Ortega <iamyoujared@MacBook-Air-de-Jared.local>
2020-05-08 03:21:06 -0500

    Add colors json

---
+++ src/utils/colors.js
@@ -0,0 +1,23 @@
+export const colors = {
+  brand: {
+    primary: "#4487BB",
+    secondary: "#5F698B",
+  },
+  action: {
+    primary: "#50C7D2",
+    secondary: "#E4F2F3",
+  },
+  backgrounds: {
+    primary: "#F0F0F0",
+    secondary: "#B0AFAD",
+  },
+  boolean: {
+    primary: "#E15964",
+    secondary: "#54DC8D",
+  },
+  essentials: {
+    light: "#FFFFFF",
+    dark: "#000000",
+    grey: "#6C6C6C",
+  },
+};
