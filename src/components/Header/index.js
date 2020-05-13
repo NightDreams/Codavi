92f740ea4cf87b31a46357d1a71f7d92bcfc56b4
JaredOrtz <jaredortega2715@gmail.com>
2020-05-13 17:32:51 -0500

    new buttons were added in the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/jsx-fragments */
+import React, { useEffect, useState, Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../../styles";
+import { ReactComponent as MenuIcon } from "../../icons/menu.svg";
 import { ReactComponent as FilterIcon } from "../../icons/filter.svg";
 import { ReactComponent as SavedIcon } from "../../icons/saved.svg";
 import logoCodavi from "../../assets/logo_codavi.svg";
@@ -11,64 +13,102 @@ import Dropdown from "../../components/Dropdown";
 import { Menu } from "../../components/Dropdown/Menu";
 import { Recent } from "../../components/Recent";
 
+// Media Query
+import { useMediaQuery } from "react-responsive";
+
 const Navegation = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 1fr;
-  height: 65px;
   align-items: center;
+  height: 65px;
   padding: 0 60px;
   background: #fff;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.04);
+  @media screen and (max-width: 576px) {
+    height: 50px;
+    padding: 0 30px;
+  }
 `;
 
 const Logo = styled.img`
   width: 115px;
+  @media screen and (max-width: 576px) {
+    width: 100px;
+    margin-top: 7px;
+  }
 `;
 
 const ItemsNav = styled.div`
   text-align: right;
 `;
 
+const ButtonMenu = styled.button`
+  position: absolute;
+  display: flex;
+  right: 24px;
+  bottom: 26px;
+  padding: 15px;
+  background: #fff;
+  border: none;
+  border-radius: 30px;
+  box-shadow: 0 64px 18px 3px rgba(0, 0, 0, 0.14),
+    0 9px 46px 8px rgba(0, 0, 0, 0.12), 0 11px 15px -7px rgba(0, 0, 0, 0.2);
+`;
+
 const Header = ({ location }) => {
+  const isMobileAndIpad = useMediaQuery({
+    query: "(max-device-width: 768px)",
+  });
   const [open, setOpen] = useState(false);
   useEffect(() => {
     setOpen(false);
   }, [location]);
 
   return (
-    <Navegation>
+    <Fragment>
+      <Navegation>
+        <div>
+          <Link to="/">
+            <Logo src={logoCodavi} alt="Codavi" />
+          </Link>
+        </div>
+        {isMobileAndIpad ? (
+          <ButtonMenu onClick={() => console.log("Show Options")}>
+            <MenuIcon />
+          </ButtonMenu>
+        ) : (
+          <ItemsNav>
+            <Link to="/saved">
+              <Button style={{ marginRight: "10px" }} type="primary">
+                <span>
+                  {" "}
+                  <SavedIcon />
+                </span>{" "}
+                Guardado
+              </Button>
+            </Link>
+            <Dropdown>
+              <Button type="primary" onClick={() => setOpen(!open)}>
+                <span>
+                  {" "}
+                  <FilterIcon />
+                </span>{" "}
+                Filtros
+              </Button>
+              {open && (
+                <Menu>
+                  <Recent />
+                </Menu>
+              )}
+            </Dropdown>
+          </ItemsNav>
+        )}
+      </Navegation>
       <div>
-        <Link to="/">
-          <Logo src={logoCodavi} alt="Codavi" />
-        </Link>
+        <p>nice one</p>
       </div>
-      <ItemsNav>
-        <Link to="/saved">
-          <Button style={{ marginRight: "10px" }} type="primary">
-            <span>
-              {" "}
-              <SavedIcon />
-            </span>{" "}
-            Guardado
-          </Button>
-        </Link>
-        <Dropdown>
-          <Button type="primary" onClick={() => setOpen(!open)}>
-            <span>
-              {" "}
-              <FilterIcon />
-            </span>{" "}
-            Filtros
-          </Button>
-          {open && (
-            <Menu>
-              <Recent />
-            </Menu>
-          )}
-        </Dropdown>
-      </ItemsNav>
-    </Navegation>
+    </Fragment>
   );
 };
 
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-fragments */
 import React, { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { GlobalStyles } from "../styles";
+import { GlobalStyles, Container } from "../styles";
 import { Home } from "../scenes/home";
 import Header from "../components/Header";
 import { CountryDetails } from "../scenes/country";
@@ -14,14 +14,14 @@ const App = () => (
     <Fragment>
       <Header />
     </Fragment>
-    <Fragment>
+    <Container>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/country/:countryCode" component={CountryDetails} />
         <Route exact path="/saved" component={Saved} />
         <Route component={NotFound} />
       </Switch>
-    </Fragment>
+    </Container>
   </BrowserRouter>
 );
 
