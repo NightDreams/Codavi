6d3d4841d21be6ecb09da273ad730050066e577b
Jair Neri <yayodcv@gmail.com>
2020-06-08 19:15:56 -0500

    Refactor listCountriesActions

--- src/actions/listCountriesActions.js
+++ src/actions/listCountriesActions.js
@@ -2,72 +2,22 @@ import axios from "axios";
 
 const url = "https://restcountries.eu/rest/v2/region";
 
-export const getAmericaCountries = () => (dispatch) => {
+const getContry = (country, TYPE, dispatch) => {
   axios
-    .get(`${url}/americas`)
-    .then((response) => {
-      dispatch({
-        type: "get_america_countries",
-        payload: response.data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+  .get(`${url}/${country}`)
+  .then((response) => {
+    dispatch({
+      type: TYPE,
+      payload: response.data,
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 };
 
-export const getAfricaCountries = () => (dispatch) => {
-  axios
-    .get(`${url}/africa`)
-    .then((response) => {
-      dispatch({
-        type: "get_africa_countries",
-        payload: response.data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-export const getAsiaCountries = () => (dispatch) => {
-  axios
-    .get(`${url}/asia`)
-    .then((response) => {
-      dispatch({
-        type: "get_asia_countries",
-        payload: response.data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-export const getEuropeCountries = () => (dispatch) => {
-  axios
-    .get(`${url}/europe`)
-    .then((response) => {
-      dispatch({
-        type: "get_europe_countries",
-        payload: response.data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-export const getOceaniaCountries = () => (dispatch) => {
-  axios
-    .get(`${url}/oceania`)
-    .then((response) => {
-      dispatch({
-        type: "get_oceania_countries",
-        payload: response.data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+export const getAmericaCountries = () => (dispatch) => getContry("americas","get_america_countries", dispatch)
+export const getAfricaCountries = () => (dispatch) => getContry("africa", "get_africa_countries", dispatch)
+export const getAsiaCountries = () => (dispatch) => getContry("asia","get_asia_countries", dispatch)
+export const getEuropeCountries = () => (dispatch) => getContry("europe", "get_europe_countries", dispatch)
+export const getOceaniaCountries = () => (dispatch) => getContry("oceania", "get_oceania_countries", dispatch)
